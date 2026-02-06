2c967e1831f506cdc0f23878eae286047ed1a719
Alegomes2 <alexandrenine@hotmail.com>
2026-02-06 01:13:55 -0300

    Controller de produtos criado, routes configurado

--- routes.js
+++ routes.js
@@ -1,10 +1,12 @@
 import { Router } from 'express'
 import UserController from './src/app/controllers/UserController.js'
 import SessionController from './src/app/controllers/SessionController.js'
+import ProductController from './src/app/controllers/ProductController.js'
 
 const routes = new Router()
 
 routes.post('/users', UserController.store)
 routes.post('/session', SessionController.store)
+routes.post('/products', ProductController.store)   
 
 export default routes;
